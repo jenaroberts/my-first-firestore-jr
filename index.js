9500ebc7f4a901b1f396bae515975f06a68eb963
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-04-29 11:16:38 -0400

    created first restaurant

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("path/to/serviceAccountKey.json");
+const serviceAccount = require("./credentials.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+const db = admin.firestore();
+
+const restaurant = {
+  name: "Mr. 01",
+  address: "555 N. Federal Hwy",
+  cuisine: "pizza",
+  rating: 4.9,
+  phoneNumber: "(786)-677-2903",
+};
+
+db.collection("restaurants")
+  .add(restaurant)
+  .then((doc) => console.log("created restaurant", doc.id))
+  .catch((err) => console.log(err));
