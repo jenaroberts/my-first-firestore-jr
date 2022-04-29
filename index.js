6d32c8d08b36b4ab64d2a72b03f6b5086e3736e9
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-04-29 11:33:57 -0400

    adding date two ways

--- index.js
+++ index.js
@@ -16,7 +16,24 @@ const restaurant = {
   phoneNumber: "(786)-677-2903",
 };
 
-db.collection("restaurants")
-  .add(restaurant)
-  .then((doc) => console.log("created restaurant", doc.id))
-  .catch((err) => console.log(err));
+// db.collection("restaurants")
+//   .add(restaurant)
+//   .then((doc) => console.log("created restaurant", doc.id))
+//   .catch((err) => console.log(err));
+
+const restaurant2 = {
+  name: "Bolay",
+  address: "7050 west palmetto parkway, boca raton",
+  cuisine: "American",
+  rating: 4.6,
+};
+
+async function addRestaurant(data) {
+  try {
+    const doc = await db.collection("restaurants").add(data);
+    console.log("created restaurant", doc.id);
+  } catch (err) {
+    console.log(err);
+  }
+}
+addRestaurant(restaurant2);
